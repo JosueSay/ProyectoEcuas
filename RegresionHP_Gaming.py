16b6e94c08e0105aa1ae25686b9916cee5efdb57
JosueSay <106031855+JosueSay@users.noreply.github.com>
2023-11-20 21:28:21 -0600

    update functions

--- RegresionHP_Gaming.py
+++ RegresionHP_Gaming.py
@@ -1,7 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
-from RegresionHP_Paviliun import modelo_enfriamiento
 
 # Nuevos datos proporcionados
 tiempo_nuevo = np.array([0, 5, 10, 15, 20, 25, 30])  # Tiempo en minutos
@@ -9,6 +8,11 @@ temperatura_nueva = np.array([49, 87, 90, 92, 86, 97, 66])  # Temperatura en °C
 temperatura_ambiente_nueva = np.mean([24.4, 25.0, 24.5, 24.5, 24.5, 24.6, 24.7])  # Temperatura ambiente promedio
 
 titulo_nuevo = 'Ajuste de la Ley de Enfriamiento de Newton computadora HP - Pavilion Gaming'
+
+
+# Función para el ajuste: Modelo de enfriamiento de Newton
+def modelo_enfriamiento(t, T0, k):
+    return temperatura_ambiente_nueva + (T0 - temperatura_ambiente_nueva) * np.exp(-k * t)
 
 # Ajuste de curva con los nuevos datos
 parametros_optimizados_nuevo, _ = curve_fit(modelo_enfriamiento, tiempo_nuevo, temperatura_nueva, p0=[49, 0.01])
@@ -19,11 +23,11 @@ print(f"Temperatura inicial ajustada (T0) con nuevos datos: {T0_ajustado_nuevo:.
 print(f"Constante de enfriamiento ajustada (k) con nuevos datos: {k_ajustado_nuevo:.4f} por minuto")
 
 # Gráfica con los nuevos datos
-temperatura_pred_nueva = modelo_enfriamiento(tiempo_pred, T0_ajustado_nuevo, k_ajustado_nuevo)
+temperatura_pred_nueva = modelo_enfriamiento(tiempo_nuevo, T0_ajustado_nuevo, k_ajustado_nuevo)
 
 plt.figure(figsize=(10, 6))
 plt.scatter(tiempo_nuevo, temperatura_nueva, color='blue', label='Datos Experimentales Nuevos')
-plt.plot(tiempo_pred, temperatura_pred_nueva, color='red', label='Ajuste del Modelo con Nuevos Datos')
+plt.plot(tiempo_nuevo, temperatura_pred_nueva, color='red', label='Ajuste del Modelo con Nuevos Datos')
 plt.title(titulo_nuevo)
 plt.xlabel('Tiempo (minutos)')
 plt.ylabel('Temperatura (°C)')
--- RegresionHP_Lenovo.py
+++ RegresionHP_Lenovo.py
@@ -1,7 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
-from RegresionHP_Paviliun import modelo_enfriamiento
 
 # Tercer conjunto de datos proporcionados
 tiempo_tercero = np.array([0, 5, 10, 15, 20, 25, 30])  # Tiempo en minutos
@@ -9,6 +8,11 @@ temperatura_tercera = np.array([47, 73, 80, 82, 83, 70, 82])  # Temperatura en �
 temperatura_ambiente_tercera = np.mean([24.4, 25.0, 24.5, 24.5, 24.5, 24.6, 24.7])  # Temperatura ambiente promedio
 
 titulo_tercero = 'Ajuste de la Ley de Enfriamiento de Newton computadora Lenovo - Legion'
+
+
+# Función para el ajuste: Modelo de enfriamiento de Newton
+def modelo_enfriamiento(t, T0, k):
+    return temperatura_ambiente_tercera + (T0 - temperatura_ambiente_tercera) * np.exp(-k * t)
 
 # Ajuste de curva con el tercer conjunto de datos
 parametros_optimizados_tercero, _ = curve_fit(modelo_enfriamiento, tiempo_tercero, temperatura_tercera, p0=[47, 0.01])
@@ -19,11 +23,11 @@ print(f"Temperatura inicial ajustada (T0) con tercer conjunto de datos: {T0_ajus
 print(f"Constante de enfriamiento ajustada (k) con tercer conjunto de datos: {k_ajustado_tercero:.4f} por minuto")
 
 # Gráfica con el tercer conjunto de datos
-temperatura_pred_tercera = modelo_enfriamiento(tiempo_pred, T0_ajustado_tercero, k_ajustado_tercero)
+temperatura_pred_tercera = modelo_enfriamiento(tiempo_tercero, T0_ajustado_tercero, k_ajustado_tercero)
 
 plt.figure(figsize=(10, 6))
 plt.scatter(tiempo_tercero, temperatura_tercera, color='blue', label='Datos Experimentales del Tercer Conjunto')
-plt.plot(tiempo_pred, temperatura_pred_tercera, color='red', label='Ajuste del Modelo con Tercer Conjunto de Datos')
+plt.plot(tiempo_tercero, temperatura_pred_tercera, color='red', label='Ajuste del Modelo con Tercer Conjunto de Datos')
 plt.title(titulo_tercero)
 plt.xlabel('Tiempo (minutos)')
 plt.ylabel('Temperatura (°C)')
